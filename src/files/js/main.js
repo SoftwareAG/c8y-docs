29866637c4818f63a94dcf863aaf9fe5a1a89b5d
Carlos Ceia <carlosceia@gmail.com>
2014-05-26 12:43:20 +0100

    styling, affix and syntax colouring with highlight.js

--- src/files/js/main.js
+++ src/files/js/main.js
@@ -1,6 +1,19 @@
 $(document).ready(function () {
+  hljs.initHighlightingOnLoad();
+
   $('[data-toggle=offcanvas]').click(function () {
-    $('.row-offcanvas').toggleClass('active')
+    $('.row-offcanvas').toggleClass('active');
   });
+
+  $('.left-nav').affix({
+    offset: {
+      top: 0
+    , bottom: function () {
+        return (this.bottom = $('footer').outerHeight(true));
+      }
+    }
+  });
+
+  $('.left-nav .active').closest('.collapse').addClass('in');
 
 });

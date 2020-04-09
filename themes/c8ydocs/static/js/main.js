5476be71279d6e46772ef4caf6f0bc53ce7afbc3
Carlos Ceia <carlosceia@gmail.com>
2020-04-09 09:44:06 +0100

    minor adjustments on releases dropdown

--- themes/c8ydocs/static/js/main.js
+++ themes/c8ydocs/static/js/main.js
@@ -2,8 +2,9 @@ var main = (function ($) {
   function initializer() {
     
     //Load releases menu
-    var json = $.getJSON({ 'url': "//cumulocity.com/guides/releases.json", 'async': false })
+    var json = $.getJSON({ 'url': "//localhost:1313/guides/releases.json", 'async': false })
       .done(function (json) {
+        //json = JSON.parse(json.responseText);
         var urls = json.releases;
         var vmenu = $('.dropdown.version');
         var loc = window.location;

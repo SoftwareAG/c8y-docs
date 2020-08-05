f094eb20fc51053ae1a87edceffdb85196680a3e
risc <risc@eur.ad.sag@SAG-DM810Z2-U.EUR.AD.SAG>
2020-08-05 15:19:31 +0200

    debugging

--- themes/c8ydocs/static/js/main.js
+++ themes/c8ydocs/static/js/main.js
@@ -15,12 +15,10 @@ var main = (function ($) {
           var el = urls[index];
           if (loc.href.includes(el.label)) {
             active = true;
-            $('#current-dropdown-version-toggle').text('ReleaseA '+ el.label);
             vmenu.find('.dropdown-menu').append(
               '<a href="' + el.url + '/about-doc/intro-documentation/" class="dropdown-menu-item active">' + el.label + '</a>'
             );
           } else {
-            $('#current-dropdown-version-toggle').text('ReleaseB '+ el.label);
             vmenu.find('.dropdown-menu').append(
               '<a href="' + el.url + '/about-doc/intro-documentation/" class="dropdown-menu-item">' + el.label + '</a>'
             );

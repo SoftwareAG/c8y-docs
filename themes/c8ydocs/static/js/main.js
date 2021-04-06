e20a493114fc9030ee209fbcdcc04f0eb3142b58
risc <risc@eur.ad.sag@SAG-DM810Z2-U.EUR.AD.SAG>
2021-04-06 17:51:22 +0200

    commented json async fetch of releases.json back in

--- themes/c8ydocs/static/js/main.js
+++ themes/c8ydocs/static/js/main.js
@@ -2,35 +2,9 @@ var main = (function ($) {
   function initializer() {
 
     //Load releases menu
-//    var json = $.getJSON({ 'url': "//cumulocity.com/guides/releases.json", 'async': false })
-//      .done(function (json) {
+    var json = $.getJSON({ 'url': "//cumulocity.com/guides/releases.json", 'async': false })
+      .done(function (json) {
         //json = JSON.parse(json.responseText);
-
-        json = {
-    "releases": [
-        {
-            "label": "10.7.0",
-            "url": "https://cumulocity.com/guides/"
-        },
-        {
-            "label": "10.6.6",
-            "url": "https://cumulocity.com/guides/10.6.6"
-        },
-        {
-            "label": "10.6.0",
-            "url": "https://cumulocity.com/guides/10.6.0"
-        },
-        {
-            "label": "10.5.7",
-            "url": "https://cumulocity.com/guides/10.5.7"
-        },
-        {
-            "label": "10.5.0",
-            "url": "https://cumulocity.com/guides/10.5.0"
-        }
-    ]
-};
-
         var urls = json.releases;
         var vmenu = $('.dropdown.version');
         var loc = window.location;
@@ -78,11 +52,11 @@ var main = (function ($) {
           nthChild.addClass('active');
           $('#current-dropdown-version-toggle').text('Release '+ nthChild.text());
         }
-//      })
-//      .fail(function (resp) {
-//        console.error(resp.statusText);
-//        $('#dropdownVersionButton').hide();
-//      });
+      })
+      .fail(function (resp) {
+        console.error(resp.statusText);
+        $('#dropdownVersionButton').hide();
+      });
 
 
     // apply Highlight js

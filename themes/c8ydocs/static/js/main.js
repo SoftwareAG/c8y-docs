83e4e5c2515dd5da86b5e9b4db1f66c1f6ff30c4
risc <risc@eur.ad.sag@SAG-DM810Z2-U.EUR.AD.SAG>
2021-04-09 16:42:01 +0200

    script update

--- themes/c8ydocs/static/js/main.js
+++ themes/c8ydocs/static/js/main.js
@@ -18,28 +18,27 @@ var main = (function ($) {
           vs.push(urls[i].label);
         }
 
-        rest = loc.href.split("guides/")[1];
+        rest = loc.href.split("/guides/")[1];
         v = rest.split("/")[0];
 
-        prefix = loc.href.split("guides/")[0] + "guides/";
+        prefix = loc.href.split("/guides/")[0] + "/guides/";
         r = rest.split("/");
         r.shift();
         suffix = r.join("/");
 
         for (var index = 0; index < urls.length; index++) {
           var el = urls[index];
-
-          console.log(el.url + "/" + suffix);
+          var href_url = el.url.endsWith('/') ? el.url + suffix : el.url + '/' + suffix;
           if (loc.href.includes(el.label)) {
             active = true;
-
             $('#current-dropdown-version-toggle').text('Release ' + el.label);
             vmenu.find('.dropdown-menu').append(
-              '<a href="' + el.url + '/' + suffix + '" class="dropdown-menu-item active">' + el.label + '</a>'
+              '<a href="' + href_url + '" class="dropdown-menu-item active">' + el.label + '</a>'
             );
           } else {
+            console.log(el.url, suffix, href_url);
             vmenu.find('.dropdown-menu').append(
-              '<a href="' + el.url + '/' + suffix + '" class="dropdown-menu-item">' + el.label + '</a>'
+              '<a href="' + href_url + '" class="dropdown-menu-item">' + el.label + '</a>'
             );
           }
         }

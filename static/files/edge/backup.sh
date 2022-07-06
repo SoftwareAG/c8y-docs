ec5bb5e0981d030566b7157eb6d08ef314b5d8b2
Sharath Krishnamurthy <shkr@softwareag.com>
2022-07-06 16:46:18 +0530

    Edge 109. Updating Edge 109 in Edge 1010 version.

--- static/files/edge/backup.sh
+++ static/files/edge/backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-# Copyright (c) 2021 Software AG, Darmstadt, Germany and/or Software AG USA Inc., Reston, VA, USA, and/or its subsidiaries and/or its affiliates and/or their licensors.
+# Copyright (c) 2018-2022 Software AG, Darmstadt, Germany and/or Software AG USA Inc., Reston, VA, USA, and/or its subsidiaries and/or its affiliates and/or their licensors.
 # Use, reproduction, transfer, publication or disclosure is prohibited except as specifically provided for in your License Agreement with Software AG.
 #
 
